c56ce0ccd17c7325eac3c7fbcc53c3315aae889e
B FAROOQ <bfarooq2022@gmail.com>
2025-08-27 09:49:07 +0530

    Analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Home } from "./Home/Home";
+import { Analytics } from "@vercel/analytics/react";
+
 
 function App() {
   return (
     <div className="bg-gradient-to-r from-transparent via-sky-200 to-transparent">
       <Home />
+      <Analytics />
     </div>
   );
 }
